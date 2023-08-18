540755524acbb793d8d6bf196689c8fa3e4cce3b
Rahul Tiwari <rahul.tiwari@Triconinfotech.com>
2023-08-18 17:04:19 +0530

    demo

---
+++ App.js
@@ -0,0 +1,4 @@
+let heading = React.createElement("h1", { id: "Heading" }, "Hello There!!.");
+console.log(heading);
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
